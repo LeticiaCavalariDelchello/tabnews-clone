4715ad9ca74343cb5fce4d5b059a0e221df16f81
Leticia Delchello <leticia.delchello@gmail.com>
2025-04-07 23:06:54 +0000

    Alterando o título

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Sua opinião REAL. Você acha que eu devo continuar aqui ou ir para outra área?</h1>
+    return <h1>O de Cereja é BEM melhor.</h1>
 }
 
 export default Home;

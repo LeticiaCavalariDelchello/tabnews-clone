abd5b914562e68631c499433fba28e40f29fff95
Leticia Delchello <leticia.delchello@gmail.com>
2025-04-15 01:04:24 +0000

    Adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>O de Cereja é BEM melhor.</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação incorreta");
-}
-
 export default Home;
